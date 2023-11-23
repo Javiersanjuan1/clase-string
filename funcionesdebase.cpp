44b2ff679cad396545fd0c9344c31820e6ab97bb
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-23 11:54:53 +0100

    trabajo

--- funcionesdebase.cpp
+++ funcionesdebase.cpp
@@ -3,3 +3,5 @@
 int main() {
     // Crear una cadena utilizando la clase string
     std::string miCadena = "Hola, esto es una cadena.";
+    // Imprimir el tamaño de la cadena usando la función size()
+    std::cout << "El tamaño de la cadena es: " << miCadena.size() << " caracteres." << std::endl;
