97373ff58748176c34645522571894d913ae9a9c
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-23 11:55:42 +0100

    trabajo

--- funcionesdebase.cpp
+++ funcionesdebase.cpp
@@ -7,3 +7,9 @@ int main() {
     std::cout << "El tamaño de la cadena es: " << miCadena.size() << " caracteres." << std::endl;
     if (miCadena.empty()) {
         std::cout << "La cadena está vacía." << std::endl;
+    } else {
+        std::cout << "La cadena no está vacía." << std::endl;
+    }
+
+    return 0;
+}
