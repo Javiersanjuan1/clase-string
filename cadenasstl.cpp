fd9589b649bd203e54e5725c39545d429f62ee14
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-23 11:52:51 +0100

    trabajo

--- cadenasstl.cpp
+++ cadenasstl.cpp
@@ -3,3 +3,7 @@
 int main() {
     // Crear un objeto de la clase string para representar una frase
     std::string frase = "Hola, este es un ejemplo de C++ con string de la STL.";
+    std::cout << "La frase es: " << frase << std::endl;
+
+    return 0;
+}
