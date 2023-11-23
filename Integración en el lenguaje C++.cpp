ea29de303c26d80abff54b41bbcdf5ee1379e8d1
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-23 11:57:37 +0100

    trabajo

--- Integración en el lenguaje C++.cpp
+++ Integración en el lenguaje C++.cpp
@@ -1,2 +1,6 @@
 #include <iostream>
 #include <string>
+int main() {
+    // Crear dos cadenas utilizando la clase string
+    std::string cadena1 = "Hola, ";
+    std::string cadena2 = "esto es una concatenacion.";
