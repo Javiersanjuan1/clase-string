919a8255a6b79a7fc88755e3bdb288bdb153aded
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-23 11:59:35 +0100

    trabajo

--- Funciones específicas de las cadenas.cpp
+++ Funciones específicas de las cadenas.cpp
@@ -4,3 +4,6 @@
 int main() {
     // Crear una cadena utilizando la clase string
     std::string miCadena = "Hola, esto es una cadena de ejemplo.";
+    // Utilizar la función find() para buscar una subcadena
+    std::string subcadenaBuscar = "cadena";
+    size_t indice = miCadena.find(subcadenaBuscar);
